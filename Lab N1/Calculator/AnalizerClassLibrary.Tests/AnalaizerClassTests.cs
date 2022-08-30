c57fda071ac5feb1ab246ea6715a3576168e52fe
markoparfeniuk <marko.parfeniuk.pm.2020@lpnu.ua>
2022-08-30 09:17:53 +0300

    add test for CreateStack (from AnalaizerClassLibrary)

---
+++ Lab N1/Calculator/AnalizerClassLibrary.Tests/AnalaizerClassTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalaizerClassLibrary.Tests
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("System.Data.SqlClient", @"Data Source=(LocalDB)\MSSQLLocalDB;Database=UnitTesting;Trusted_Connection=True;MultipleActiveResultSets=true", "TestData", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void CreateStack_FromDataSourceTest()
+        {
+            // Arrange
+            string initial = (string)TestContext.DataRow["initial"];
+            string expected = (string)TestContext.DataRow["expected"];
+            AnalaizerClass.expression = initial;
+
+            // Actual
+            string actual = string.Join("", AnalaizerClass.CreateStack().ToArray());
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Lab N1/Calculator/AnalizerClassLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerClassLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerClassLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b772cd10-a00e-4826-9832-3db436935d71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
